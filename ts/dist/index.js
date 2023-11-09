adcdc8ccef47fdb67b369f5d8da0512c3189958c
Dominic <dominicezeifejafor@gmail.com>
2023-11-09 13:33:30 +0100

    typescript tutorials

--- ts/dist/index.js
+++ ts/dist/index.js
@@ -11,5 +11,77 @@ console.log(arrayOfIds);
 console.log('ID:', id);
 console.log('NAME:', secondId);
 console.log('FullName:', fullname);
-// Tuple
-const tupleArray = [10, 'abc', true, 'gh'];
+// TUPLE (Used to list out types in a single array)
+const tuple = [10, 'abc', true, 'gh'];
+// TUPLEARRAY (Used for an array. Could be Array of arrays or Array of objects etc)
+const employee = [
+    [1, 'Brady', 'NFL'],
+    [2, 'LeBron', 'NBA'],
+    [3, 'Kobe', 'NBA']
+];
+// UNION (Different types for one variable)
+const unionId = 45;
+// ENUM (Enumerated type)
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+console.log(Direction.Down);
+var Direction1;
+(function (Direction1) {
+    Direction1["Up"] = "up";
+    Direction1["Down"] = "down";
+    Direction1["Right"] = "right";
+    Direction1["Left"] = "left";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Right);
+const user = [
+    {
+        id: 1,
+        name: 'Jordan',
+        classStatus: true
+    },
+    {
+        id: 2,
+        name: 'LBJ',
+        classStatus: true
+    },
+    {
+        id: 3,
+        name: 'K. Brown',
+        classStatus: false
+    }
+];
+// TYPE ASSERTION
+const cId = 1;
+// const customerId = <number>cId
+const customerId = cId;
+// Both ways work
+// FUNCTIONS
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(50, 67));
+// VOID
+function logs(message) {
+    return message;
+}
+console.log(logs('90 is a number'));
+const users = [
+    {
+        id: 1,
+        name: 'Karl Malone'
+    },
+    {
+        id: 2,
+        name: 'John Stockton'
+    }
+];
+const singleUser = {
+    id: 22,
+    name: 'Stallone Sylvester'
+};
+console.log('Multiple users', users, 'Single user', singleUser);
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -15,7 +15,99 @@ console.log('ID:', id);
 console.log('NAME:', secondId);
 console.log('FullName:', fullname);
 
-// Tuple
-const tupleArray: [number, string, boolean, string] = [10, 'abc', true, 'gh']
+// TUPLE (Used to list out types in a single array)
+const tuple: [number, string, boolean, string] = [10, 'abc', true, 'gh']
 
-// Union
+// TUPLEARRAY (Used for an array. Could be Array of arrays or Array of objects etc)
+const employee: [number, string, string][] = [
+    [1, 'Brady', 'NFL'],
+    [2, 'LeBron', 'NBA'],
+    [3, 'Kobe', 'NBA']
+]
+
+// UNION (Different types for one variable)
+const unionId: string | number = 45
+
+// ENUM (Enumerated type)
+enum Direction {
+    Up,
+    Down,
+    Left, 
+    Right
+}
+console.log(Direction.Down);
+
+enum Direction1 {
+    Up = 'up',
+    Down = 'down',
+    Right = 'right',
+    Left = 'left'
+}
+console.log(Direction1.Right);
+
+type User = {
+    id: number,
+    name: string,
+    classStatus: boolean
+}
+const user: User[] = [
+    {
+        id: 1,
+        name: 'Jordan',
+        classStatus: true
+    },
+    {
+        id: 2,
+        name: 'LBJ',
+        classStatus: true
+    },
+    {
+        id: 3,
+        name: 'K. Brown',
+        classStatus: false
+    }
+]
+
+// TYPE ASSERTION
+const cId: any = 1
+// const customerId = <number>cId
+const customerId = cId as number
+// Both ways work
+
+// FUNCTIONS
+function addNum(x: number, y: number) {
+    return x + y
+}
+console.log(addNum(50, 67));
+
+// VOID
+function logs(message: string | number) {
+    return message
+}
+console.log(logs('90 is a number'));
+
+// INTERFACES (Can't be used with unions)
+interface Users {
+    id: number,
+    name: string
+}
+
+const users: Users[] = [
+    {
+        id: 1,
+        name: 'Karl Malone'
+    },
+    {
+        id: 2,
+        name: 'John Stockton'
+    }
+]
+
+const singleUser: Users = {
+    id: 22,
+    name: 'Stallone Sylvester'
+}
+console.log('Multiple users', users, 'Single user', singleUser);
+
+
+
