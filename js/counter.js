3de21d3efa9e963fff906f047ccafce195b57290
Dominic <dominicezeifejafor@gmail.com>
2023-11-15 19:15:50 +0100

    Created a counter

---
+++ js/counter.js
@@ -0,0 +1,15 @@
+const add = document.querySelector('.incrementBtn')
+const minus = document.querySelector('.decrementBtn')
+const displayValue = document.querySelector('.counterValue')
+
+let value = 0
+add.addEventListener('click', incrementValue => {
+    value += 1
+    displayValue.textContent = value
+})
+
+minus.addEventListener('click', decrementValue => {
+    value -= 1
+    displayValue.textContent = value
+})
+console.log(add, minus);
