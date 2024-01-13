373cf2d8d7cda271defd2b127f12d5b2dbf10a65
Dominic <dominicezeifejafor@gmail.com>
2024-01-13 17:44:18 +0100

    array helper methods

--- js/loops.js
+++ js/loops.js
@@ -1,32 +1,34 @@
 "use strict";
-    // FOR LOOPS
+// FOR LOOPS
+const smallArray = ["first", "second", "third", "Draxler", "Vitinha"];
+const smallNums = [ , 5, 7]
+for (let i = 0; i < smallArray.length; i++) {
+    // i is initially assigned zero because index of the first element in an array is zero
+    console.log(smallArray[i]);
+}
+for (let i = 0; i < 41; i++) {
+    console.log(i);
+}
+for ( let i = 0; i < 16; i++) {
+    console.log(`Hello Mothefuckers! ${i}`);
+}
+for (let i = 0; i < smallNums.length; i++) {
+    console.log(smallNums[i] * smallNums[i]);
+}
 
-    const smallArray = ["first", "second", "third", "Draxler", "Vitinha"];
-    for (let i = 0; i < smallArray.length; i++) {
-        // i is initially assigned zero because index of the first element in an array is zero
-        console.log(smallArray[i]);
-    }
-
-    for (let i = 0; i < 41; i++) {
-        console.log(i);
-    }
-    for ( let i = 0; i < 16; i++) {
-        console.log(`Hello Mothefuckers! ${i}`);
-    }
 
 
+// WHILE LOOPS
+let x = 0;
+while (x < 15) {
+    console.log(x);
+    x++;
+}
 
-    // WHILE LOOPS
+let a = 0;
+const whileArray = [2, 3, true, undefined, "selective", "Albeciete", "Selacao", {carBrand: "Tesla", price: 30000, year: 1988, owner: "Elon Musk"}];
+while (a < whileArray.length) {
+    console.log(whileArray[a]);
+    a++;
+}
 
-    let x = 0;
-    while (x < 15) {
-        console.log(x);
-        x++;
-    }
-
-    let a = 0;
-    const whileArray = [2, 3, true, undefined, "selective", "Albeciete", "Selacao", {carBrand: "Tesla", price: 30000, year: 1988, owner: "Elon Musk"}];
-    while (a < whileArray.length) {
-        console.log(whileArray[a]);
-        a++;
-    }
