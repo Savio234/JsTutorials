7bc40bcbbbba4980fca9b3ae500c3504fcf88b39
Dominic <dominicezeifejafor@gmail.com>
2024-03-12 12:42:42 +0100

    changes

---
+++ redux/index.js
@@ -0,0 +1,7 @@
+let username = '   Harley   '
+let message = `Hello ${username.trim()}, Good morning`
+console.log(message);
+
+const trim = name => name.trim()
+const generateMessgae = name => `Hello ${name}, Good morning`
+const convertToUpper = name => name.toUpperCase()
